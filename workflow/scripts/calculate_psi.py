bcc21eda772f4c896dd20ff7511b365614ece0b9
niekwit <41302859+niekwit@users.noreply.github.com>
2024-11-25 11:40:34 +0000

    added hit identification

--- workflow/scripts/calculate_psi.py
+++ workflow/scripts/calculate_psi.py
@@ -12,12 +12,15 @@ logging.basicConfig(filename=log, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Load Snakemake variables
-MIN_SOB_THRESHOLD = snakemake.config["bins"]["sob_threshold"]
-comparisons = pd.read_csv("config/stats.csv")
-REFERENCE_CONDITIONS  = comparisons["control_condition"]
-TEST_CONDITIONS = comparisons["test_condition"]
-OUTPUT_FILES_ALL = snakemake.output["all"]
-OUTPUT_FILES_COLLAPSED = snakemake.output["coll"]
+counts = snakemake.input["counts"]
+MIN_SOB_THRESHOLD = snakemake.config["psi"]["sob_threshold"]
+comparisons = snakemake.wildcards["comparison"]
+REFERENCE_CONDITIONS = [x.split("_vs_")[1] for x in comparisons]
+TEST_CONDITIONS = [x.split("_vs_")[0] for x in comparisons]
+stab_th = snakemake.config["psi"]["stringent_hit_threshold"]
+destab_th = snakemake.config["psi"]["stringent_negative_hit_threshold"]
+sd_th = snakemake.config["psi"]["sd_th"]
+OUTPUT_FILES = snakemake.output
 
 
 def compute_psi(row, condition, num_bins):
@@ -33,7 +36,7 @@ def compute_psi(row, condition, num_bins):
 
 
 # Read count for all samples
-df_all = pd.read_csv(snakemake.input["counts"], sep="\t")
+df_all = pd.read_csv(counts, sep="\t")
 
 # Process data for each comparison separately
 for reference, test in zip(REFERENCE_CONDITIONS, TEST_CONDITIONS):
@@ -41,9 +44,13 @@ for reference, test in zip(REFERENCE_CONDITIONS, TEST_CONDITIONS):
     
     # Select columns that are part of the comparison
     df = df_all.filter(regex=f"{reference}|{test}|^barcode|^gene")
+    nrows = df.shape[0]
+    logger.info(f" Barcodes present pre-filtering: {nrows}")
         
-    # Remove rows where there are no counts accross all samples
+    # Remove barcodes where there are no counts accross all samples
     df = df[df.iloc[:,2:].sum(axis=1) > 0].reset_index(drop=True)
+    nrows_zero_counts = nrows - df.shape[0]
+    logger.info(f" Barcodes with no counts in any sample: {nrows_zero_counts}")
 
     # Identify the sample with the most reads 
     largest_sum = df.iloc[:,2:].sum().max()
@@ -65,11 +72,14 @@ for reference, test in zip(REFERENCE_CONDITIONS, TEST_CONDITIONS):
         sample_sums[f"SOB_{sample}"] = df.filter(regex=f"^{sample}_").sum(axis=1)
     df = pd.concat([df, pd.DataFrame(sample_sums)], axis=1)  
     
-   # Remove rows where there are low counts in the reference sample
+   # Remove barcodes where there are low counts in the reference sample
     df = df[df[f"SOB_{reference}"] > MIN_SOB_THRESHOLD].reset_index()
+    nrows_low_counts = nrows_zero_counts - df.shape[0]
+    logger.info(f" Barcodes with low counts in {reference}: {nrows_low_counts} ")
 
-    # Remove rows with no test count reads in any bin (to avoid division by zero)
+    # Remove barcodes with no test count reads in any bin (to avoid division by zero)
     df = df[df.filter(regex=f"{test}_").sum(axis=1) > 0].reset_index(drop=True)
+    nrows_no_test_counts = nrows_low_counts - df.shape[0]
     
     # Compute PSI values:
     # For each sample bin, divide the bin count by the sum of all bins for that sample
@@ -99,6 +109,11 @@ for reference, test in zip(REFERENCE_CONDITIONS, TEST_CONDITIONS):
     # Add total number of barcodes for each ORF
     df["num_barcodes"] = df.groupby("orf")["barcode"].transform("count")
     
+    # Remove ORFs with only one barcode
+    df = df[df["num_barcodes"] > 1].reset_index(drop=True)
+    nrows_single_barcode = nrows_no_test_counts - df.shape[0]
+    logger.info(f" ORFs removed that have only one barcode after filtering: {nrows_single_barcode}")
+    
     # Sum of read counts for each ORF and each bin
     for sample in [reference, test]:
         # Get columns with bin counts
@@ -107,8 +122,32 @@ for reference, test in zip(REFERENCE_CONDITIONS, TEST_CONDITIONS):
         for bin in sample_bins:
             df[bin] = df.groupby("orf")[bin].transform("sum")
     
+    # Identify ORFs that are stabilised in test condition
+    df[f"stabilised_in_{test}"] = df["delta_PSI_mean"] > stab_th
+    sum_ = df[["orf", f"stabilised_in_{test}"]].drop_duplicates()
+    sum_ = sum_[f"stabilised_in_{test}"].sum()
+    logger.info(f" Number of stabilised ORFs in {test}: {sum_}")
+    
+    # Identify high confidence hits for stabilised ORFs
+    df[f"stabilised_in_{test}_hc"] = (df["delta_PSI_mean"] > stab_th) & (df["delta_PSI_mean"] > sd_th * df["delta_PSI_SD"])
+    sum_ = df[["orf", f"stabilised_in_{test}_hc"]].drop_duplicates()
+    sum_ = sum_[f"stabilised_in_{test}_hc"].sum()
+    logger.info(f" Number of high confidence stabilised ORFs in {test}: {sum_}")
+    
+    # Identify ORFs that are destabilised in test condition
+    df[f"destabilised_in_{test}"] = df["delta_PSI_mean"] < destab_th
+    sum_ = df[["orf", f"destabilised_in_{test}"]].drop_duplicates()
+    sum_ = sum_[f"destabilised_in_{test}"].sum()
+    logger.info(f" Number of destabilised ORFs in {test}: {sum_}")
+    
+    # Identify high confidence hits for destabilised ORFs
+    df[f"destabilised_in_{test}_hc"] = (df["delta_PSI_mean"] < destab_th) & (df["delta_PSI_mean"] > sd_th * df["delta_PSI_SD"])
+    sum_ = df[["orf", f"destabilised_in_{test}_hc"]].drop_duplicates()
+    sum_ = sum_[f"destabilised_in_{test}_hc"].sum()
+    logger.info(f" Number of high confidence destabilised ORFs in {test}: {sum_}")
+    
     # Write to file
-    file = [x for x in OUTPUT_FILES_ALL if f"{test}_vs_{reference}" in x][0]
+    file = [x for x in OUTPUT_FILES if f"{test}_vs_{reference}" in x][0]
     logger.info(f" Writing to {file}")
     df.to_csv(file, index=False)
 
