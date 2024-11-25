fe71d616b8d818fb4336e511b3fda2ab7b8eedb3
niekwit <41302859+niekwit@users.noreply.github.com>
2024-11-25 16:49:16 +0000

    Initial commit

---
+++ workflow/scripts/csv_to_fasta.py
@@ -0,0 +1,25 @@
+import glob
+import pandas as pd
+
+"""
+Convert csv file to fasta file. Barcode name and sequence 
+column numbers must be specified in config.yml.
+"""
+
+barcode_column = snakemake.config["csv"]["barcode_id_column"]
+sequence_column = snakemake.config["csv"]["sequence_column"]
+
+# Load csv file
+df = pd.read_csv(snakemake.input["csv"])
+
+# Only keep barcode ID and sequence columns
+df = df.iloc[:, [barcode_column, sequence_column]]
+
+# Prepend ">" to the barcode ID
+df.iloc[:, 0] = ">" + df.iloc[:, 0]
+
+# Write fasta file
+df.to_csv(snakemake.output["fasta"],
+          sep="\n",
+          index=False,
+          header=False)
