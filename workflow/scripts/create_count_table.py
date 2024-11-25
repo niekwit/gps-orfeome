381bfec158609e885339df92a44a6e8769550a1f
niekwit <41302859+niekwit@users.noreply.github.com>
2024-11-25 08:16:48 +0000

    adapted for (non)-MAGeCK use

--- workflow/scripts/create_count_table.py
+++ workflow/scripts/create_count_table.py
@@ -2,47 +2,45 @@ import os
 import pandas as pd
 
 ''' 
-Join count files to create count table
+Merge count files to create count table.
+With bin_number = 1,
+MAGeCK requires the first two columns to be sgRNA and gene.
+Will be renamed to barcode and ORF, respectively post-MAGeCK.
+
+Otherwise, the count table will be created with the first column as the barcode name
+and the second the ORF name.
 '''
 
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+if snakemake.config["bin_number"] == 1:
+    # For use with MAGeCK
+    name = "sgRNA"
+    gene = "gene"
+else:
+    name = "barcode"
+    gene = "gene"
 
-counts = {}
+# Read all barcode names from csv and create ORF column
+df = pd.read_csv(snakemake.input["csv"], header=None)
+df = df[snakemake.config["csv"]["barcode_column"] - 1]
+df[gene] = df[0].str.rsplit(pat="_", n=1, expand=True)[0]
+
+# Rename first column 
+df = df.rename(columns={0: name})
+
+# Merge all count files to df to create count table
+for file in snakemake.input["files"]:
+    # Read count file
+    tmp = pd.read_csv(file, sep=" ", header=None)
     
-for file in snakemake.input:
-    # Check if file size is larger than 0
-    assert os.path.getsize(file) > 0, f"{file} is empty (alignment failed?)"
+    # Rename headers to wildcard value (for counts) and sgRNA 
+    wildcard_value = os.path.basename(file).replace(".barcode.counts.txt", "")
+    tmp.columns = [wildcard_value, name]
     
-    # Add counts to counts dict
-    df = pd.read_csv(file,sep=" ",header = None)
-    key = os.path.basename(file).replace(".barcode.counts.txt", "")
-    df.columns = [key, "barcode"]
-    df[key].astype(int)
-    counts.update({key:df})
+    # Merge to df
+    df = pd.merge(df, tmp, on=name, how="left")
 
-df = pd.read_csv(snakemake.params[0], header = None)
-df.columns = ["barcode"]
-
-df = df[df["barcode"].str.contains(">")]
-df = df.reset_index(drop = True)
-df["barcode"] = df["barcode"].str.replace(">", "")
-df["gene"] = df["barcode"].str.split(pat = "_",n = 1,expand = True)[0]
-
-# Perform left join on count files
-for key, value in counts.items():
-    df = pd.merge(df, value, on='barcode', how='left')
-df["barcode"] = df["barcode"].str.split(pat = "_",n = 1,expand = True)[1]
-
-# Replace nan with zero
+# Replace missing values with zero
 df = df.fillna(0)
-df = df.sort_values(by = ["barcode"])
-
-# Convert floats back to int after pandas merge (bug in pandas)
-index_range = range(2, len(df.columns))
-index_list = []
-for i in index_range:
-    index_list.append(i)
-df[df.columns[index_list]] = df[df.columns[index_list]].astype(int)
 
 # Save data frame to file
-df.to_csv(snakemake.output[0], sep = '\t', index = False)
+df.to_csv(snakemake.output[0], sep='\t', index=False)
