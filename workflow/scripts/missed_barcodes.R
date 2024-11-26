d4e75ae608ec28b8e318d737afc414995ced47b5
niekwit <41302859+niekwit@users.noreply.github.com>
2024-11-26 14:49:00 +0000

    y axis name barcode

--- workflow/scripts/missed_barcodes.R
+++ workflow/scripts/missed_barcodes.R
@@ -1,4 +1,4 @@
-# redirect R output to log
+# Redirect R output to log
 log <- file(snakemake@log[[1]], open="wt")
 sink(log, type = "output")
 sink(log, type = "message")
@@ -7,30 +7,30 @@ library(tidyverse)
 library(reshape2)
 library(cowplot)
 
-# load count table
+# Load count table
 counts <- read.delim(snakemake@input[[1]]) %>%
   dplyr::select(-c("sgRNA", "gene"))
 
-# get number of sgRNAs with zero counts
+# Get number of sgRNAs with zero counts
 df <- colSums(counts == 0) %>%
   melt() %>%
   rownames_to_column(var = "sample") 
 
-# create bar graph
+# Create bar graph
 p <- ggplot(data = df, aes(x = sample, y = value)) + 
   geom_bar(stat = "identity",
            fill = "#419179",
            colour = "black") +
   theme_cowplot(16) +
   xlab(NULL) +
-  ylab("Missed sgRNAs") +
+  ylab("Missed barcodes") +
   scale_y_continuous(expand = expansion(mult = c(0, 0.1))) +
   scale_x_discrete(guide = guide_axis(angle = 45))
 
-# save plot
+# Save plot
 ggsave(snakemake@output[[1]], p)
 
-# close redirection of output/messages
+# Close redirection of output/messages
 sink(log, type = "output")
 sink(log, type = "message")
 
