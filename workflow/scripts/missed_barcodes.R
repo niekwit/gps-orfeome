ba3dbd51cf644df668837fc38a3f7c0dc4f3e18e
niekwit <41302859+niekwit@users.noreply.github.com>
2024-12-12 11:49:14 +0000

    plot zeros of sum of all bins

--- workflow/scripts/missed_barcodes.R
+++ workflow/scripts/missed_barcodes.R
@@ -12,16 +12,41 @@ bin.number <- snakemake@params[["bin_number"]]
 # Load count table
 if (bin.number == 1) {
   counts <- read.delim(snakemake@input[[1]]) %>%
-  dplyr::select(-all_of(c("sgRNA", "gene")))
+    dplyr::select(-all_of(c("sgRNA", "gene")))
+  
+  # Get number of sgRNAs with zero counts
+  df <- colSums(counts == 0) %>%
+    melt() %>%
+    rownames_to_column(var = "sample")
 } else {
   counts <- read.delim(snakemake@input[[1]]) %>%
-  dplyr::select(-all_of(c("barcode_id", "orf_id", "gene")))
+    dplyr::select(-all_of(c("barcode_id", "orf_id", "gene")))
+  
+  # Get unique conditions (no bin number)
+  conditions <- unique(gsub("_[0-9]$", "", names(counts)))
+  
+  # Sum of all the columns that contain a condition
+  for (c in conditions) {
+    counts[[c]] <- rowSums(counts %>% select(contains(c)))
+  }
+  # Only keep the summed columns
+  counts <- counts %>% select(all_of(conditions))
+  
+  # Get number of sgRNAs with zero counts for each condition
+  zeros.count <- colSums(counts ==0) 
+    
+  # Count rows where all values are zero
+  zeros.count["Missing in\nall conditions"] <- sum(rowSums(counts == 0) == ncol(counts))
+  
+  # Convert to data frame for plotting
+  df <- data.frame(
+    sample = names(zeros.count),
+    value = as.numeric(zeros.count)
+  )
+  
+  # Change order factor levels of sample column so that "Missing in all conditions" is last
+  df$sample <- factor(df$sample, levels = c(conditions, "Missing in\nall conditions"))
 }
-
-# Get number of sgRNAs with zero counts
-df <- colSums(counts == 0) %>%
-  melt() %>%
-  rownames_to_column(var = "sample") 
 
 # Create bar graph
 p <- ggplot(data = df, aes(x = sample, y = value)) + 
