2d6973490203c2a99b8b3b3bb25c9f574239e9be
niekwit <41302859+niekwit@users.noreply.github.com>
2026-05-27 08:53:55 +0100

    added ... parameter for more flexibility

--- workflow/scripts/theme_gpsw.R
+++ workflow/scripts/theme_gpsw.R
@@ -3,8 +3,8 @@ library(cowplot)
 GPSW_COLOUR <- "#419179"
 GPSW_BASE_SIZE <- 16
 
-theme_gpsw <- function() {
-  theme_cowplot(GPSW_BASE_SIZE)
+theme_gpsw <- function(...) {
+  theme_cowplot(GPSW_BASE_SIZE, ...)
 }
 
 geom_bar_gpsw <- function(...) {
